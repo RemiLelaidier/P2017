e465700c9e994e3738829b1f34901d14ee8da4fb
Remi Lelaidier <remi.lelaidier1@etu.unice.fr>
2017-03-08 15:29:17 +0100

    JSON transmit fini + style

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+/**
+ * Fonction pour charger les themes
+ * @param themes
+ */
 function loadThemes(themes) {
-    console.log(themes)
+    for (i = 0; i < themes.length; i++){
+        var tr = document.createElement('TR')
+        for(j = 0; j < 5; j++){
+            td = document.createElement('TD')
+            td.innerHTML = themes[j].titre
+            tr.appendChild(td)
+        }
+        document.getElementById('liste').appendChild(tr)
+    }
 }
