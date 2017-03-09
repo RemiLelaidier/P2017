34973f76190f5aed984ed7988f023f394d50c342
Remi Lelaidier <remi.lelaidier1@etu.unice.fr>
2017-03-09 08:19:22 +0100

    Style fait

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,37 @@
  * @param themes
  */
 function loadThemes(themes) {
-    for (i = 0; i < themes.length; i++){
+    for (i = 0; i < themes.length / 5; i++){
         var tr = document.createElement('TR')
         for(j = 0; j < 5; j++){
-            td = document.createElement('TD')
-            td.innerHTML = themes[j].titre
+
+            // identifieur JSON
+            identifieur = i*5 + j
+
+            var td = document.createElement('TD')
+            td.innerHTML = themes[identifieur].titre
+            td.title = themes[identifieur].description
+
+            // Couleurs
+            if(themes[identifieur].couleur === "BLUE")
+                td.style.backgroundColor = "#77c9d2"
+            if(themes[identifieur].couleur === "VERT")
+                td.style.backgroundColor = "#bbdeab"
+            if(themes[identifieur].couleur === "INDIGO")
+                td.style.backgroundColor = "#acb9dd"
+            if(themes[identifieur].couleur === "VIOLET")
+                td.style.backgroundColor = "#d49fb8"
+            if(themes[identifieur].couleur === "JAUNE")
+                td.style.backgroundColor = "#fed899"
+            if(themes[identifieur].couleur === "ORANGE")
+                td.style.backgroundColor = "#f9b98f"
+            if(themes[identifieur].couleur === "ROUGE")
+                td.style.backgroundColor = "#f59888"
+
+            td.addEventListener("click", function () {
+                td.className = "selected"
+            })
+
             tr.appendChild(td)
         }
         document.getElementById('liste').appendChild(tr)
